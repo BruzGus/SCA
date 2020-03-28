240257fe8be7f187335b87a1841477806051064e
Bryan Agusto Cruz Castro <bryan.cc.652@gmail.com>
2020-03-28 17:04:27 -0400

    Creacion del modelo tecnico *Se realizo el desarrollo del modelo tecnico para el manejo de los DML,
    TAREA CREACION DE MODELOS EN ASANA.
    para el proyecto.

---
+++ models/tecnico.go
@@ -0,0 +1,18 @@
+package models
+
+import "github.com/jinzhu/gorm"
+
+//Tecnico es un modelo para la entidad TECNICOS
+//de nuestra base de datos SCA y para poder realizar
+//las consulta DML.
+type Tecnico struct {
+	gorm.Model
+	ID       string `json:"id" gorm:"not null"`
+	Nombre   string `json:"nombre" gorm:"not null"`
+	Apaterno string `json:"apaterno" gorm:"not null"`
+	Amaterno string `json:"amaterno" gorm:"not null"`
+	Usuario  string `json:"usuario"`
+	Email    string `json:"email"`
+	Celular  uint16 `json:"celular"`
+	Pin      string `json:"ping,omitempty"`
+}
